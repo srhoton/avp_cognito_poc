320eb49f6bbd492d560735a252cafd16cc19dc34
Steve Rhoton <steve.rhoton@fullbay.com>
2025-02-14 14:52:41 -0500

    Stubbed in JKWS Validation

--- user-authorizer/src/main/java/com/fullbay/ath/UserAuthorizer.java
+++ user-authorizer/src/main/java/com/fullbay/ath/UserAuthorizer.java
@@ -13,12 +13,23 @@ import com.fasterxml.jackson.core.util.RecyclerPool.WithPool;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.source.*;
+import com.nimbusds.jose.proc.BadJOSEException;
+import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.jwt.*;
 import com.nimbusds.jwt.proc.*;
 import jakarta.inject.Named;
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+import java.security.*;
 import java.security.Policy;
+import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 @Named("authorizer")
@@ -36,7 +47,9 @@ public class UserAuthorizer
         logger.log("Received context: " + context);
 
         Map<String, String> headers = event.getHeaders();
-
+        String token = headers.get("Authorization");
+        logger.log("Received token: " + token);
+        logger.log("Token is valid: " + isTokenValid(token, logger));
         Statement statement = IamPolicyResponseV1.allowStatement(
             "arn:aws:iam::123456789012:user/example-user"
         );
@@ -52,9 +65,63 @@ public class UserAuthorizer
         return response;
     }
 
-    private Boolean isTokenValid(String token) {
+    private Boolean isTokenValid(String token, LambdaLogger logger) {
         try {
-            JWSObject jwsObject = JWSObject.parse(token);
+            URI keyUri = new URI(
+                "https://" + System.getenv("key_domain") + "/jwks.json"
+            );
+            URL keyUrl = keyUri.toURL();
+            ConfigurableJWTProcessor<SecurityContext> jwtProcessor =
+                new DefaultJWTProcessor<>();
+            jwtProcessor.setJWSTypeVerifier(
+                new DefaultJOSEObjectTypeVerifier<>(
+                    new JOSEObjectType("at+jwt")
+                )
+            );
+            JWKSource<SecurityContext> keySource = JWKSourceBuilder.create(
+                keyUrl
+            )
+                .retrying(true)
+                .build();
+            JWSAlgorithm expectedJWSAlgorithm = JWSAlgorithm.RS256;
+            JWSKeySelector<SecurityContext> keySelector =
+                new JWSVerificationKeySelector<>(
+                    expectedJWSAlgorithm,
+                    keySource
+                );
+
+            jwtProcessor.setJWSKeySelector(keySelector);
+            jwtProcessor.setJWTClaimsSetVerifier(
+                new DefaultJWTClaimsVerifier<>(
+                    new JWTClaimsSet.Builder()
+                        .issuer("https://" + System.getenv("key_domain"))
+                        .build(),
+                    new HashSet<>(
+                        Arrays.asList(
+                            JWTClaimNames.SUBJECT,
+                            JWTClaimNames.ISSUED_AT,
+                            JWTClaimNames.EXPIRATION_TIME,
+                            "scp",
+                            "cid",
+                            JWTClaimNames.JWT_ID
+                        )
+                    )
+                )
+            );
+
+            SecurityContext ctx = null;
+            JWTClaimsSet claimsSet;
+            try {
+                claimsSet = jwtProcessor.process(token, ctx);
+            } catch (ParseException | BadJOSEException e) {
+                logger.log("Error processing JWT token");
+                return false;
+            } catch (JOSEException e) {
+                logger.log("Error processing JWT token");
+                return false;
+            }
+            logger.log("JWT token processed successfully");
+            logger.log("JWT claims set: " + claimsSet.toJSONObject());
             return true;
         } catch (Exception e) {
             return false;
