8904f1695f3cd113228f7e3ddb3cb081c590582c
Steve Rhoton <steve.rhoton@fullbay.com>
2025-02-15 11:33:12 -0500

    Added lambda configuration

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -8,12 +8,12 @@ resource "aws_lambda_function" "user_lambda" {
   role          = aws_iam_role.user_lambda_role.arn
   handler       = "io.quarkus.amazonaws.lambda.runtime.QuarkusStreamHandler::handleRequest"
   runtime       = "java21"
-  filename      = "../user_authorizer/com.fullbay.ath.userauthorizer/target/function.zip"
+  filename      = "../user-authorizer/build/function.zip"
   timeout       = 900
   memory_size   = 512
   logging_config {
-    log_group = aws_cloudwatch_log_group.user_lambda.name
+    log_group  = aws_cloudwatch_log_group.user_lambda.name
     log_format = "Text"
   }
-  depends_on = [ aws_cloudwatch_log_group.user_lambda ]
+  depends_on = [aws_cloudwatch_log_group.user_lambda]
 }
