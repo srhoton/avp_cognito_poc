5f52871f32bcc49bebdd79cde568b9ded8d5044b
Steve Rhoton <steve.rhoton@fullbay.com>
2025-02-15 19:34:35 -0500

    Authorizer is cabled up for testing

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -1,13 +1,13 @@
 resource "aws_api_gateway_rest_api" "avp_user_gateway" {
   name        = "avp-user-gateway"
-  description = "API Gateway for AVP User Service" 
+  description = "API Gateway for AVP User Service"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 resource "aws_api_gateway_rest_api" "avp_machine_gateway" {
   name        = "avp-machine-gateway"
-  description = "API Gateway for AVP Machine Service" 
+  description = "API Gateway for AVP Machine Service"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
@@ -23,14 +23,15 @@ resource "aws_api_gateway_method" "avp_user_method" {
   rest_api_id   = aws_api_gateway_rest_api.avp_user_gateway.id
   resource_id   = aws_api_gateway_resource.avp_user_resource.id
   http_method   = "GET"
-  authorization = "NONE"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.avp_user_authorizer.id
 }
 
 resource "aws_api_gateway_integration" "user_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.avp_user_gateway.id
-  resource_id             = aws_api_gateway_resource.avp_user_resource.id
-  http_method             = aws_api_gateway_method.avp_user_method.http_method
-  type                    = "MOCK"
+  rest_api_id = aws_api_gateway_rest_api.avp_user_gateway.id
+  resource_id = aws_api_gateway_resource.avp_user_resource.id
+  http_method = aws_api_gateway_method.avp_user_method.http_method
+  type        = "MOCK"
 }
 
 resource "aws_api_gateway_resource" "avp_machine_resource" {
@@ -47,8 +48,16 @@ resource "aws_api_gateway_method" "avp_machine_method" {
 }
 
 resource "aws_api_gateway_integration" "machine_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.avp_machine_gateway.id
-  resource_id             = aws_api_gateway_resource.avp_machine_resource.id
-  http_method             = aws_api_gateway_method.avp_machine_method.http_method
-  type                    = "MOCK"
+  rest_api_id = aws_api_gateway_rest_api.avp_machine_gateway.id
+  resource_id = aws_api_gateway_resource.avp_machine_resource.id
+  http_method = aws_api_gateway_method.avp_machine_method.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_authorizer" "avp_user_authorizer" {
+  rest_api_id            = aws_api_gateway_rest_api.avp_user_gateway.id
+  name                   = "avp_user_authorizer"
+  authorizer_uri         = aws_lambda_function.user_lambda.invoke_arn
+  authorizer_credentials = aws_iam_role.user_lambda_role.arn
+  type                   = "REQUEST"
 }
--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -25,16 +25,58 @@ data "aws_iam_policy_document" "user_lambda_assume_role" {
   }
 }
 
+data "aws_iam_policy_document" "invocation_policy" {
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "lambda:InvokeFunction",
+    ]
+
+    resources = [
+      aws_lambda_function.user_lambda.arn
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "invocation_assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_policy" "user_lambda_logging" {
-  policy = data.aws_iam_policy_document.user_lambda_logging.json
-  name   = "user_lambda_logging"
-  path   = "/"
+  policy      = data.aws_iam_policy_document.user_lambda_logging.json
+  name        = "user_lambda_logging"
+  path        = "/"
   description = "IAM policy for user lambda logging"
+}
+
+resource "aws_iam_policy" "user_lambda_invocation" {
+  policy      = data.aws_iam_policy_document.invocation_policy.json
+  name        = "user_lambda_invocation"
+  path        = "/"
+  description = "IAM policy for user lambda invocation"
 }
 
 resource "aws_iam_role" "user_lambda_role" {
   name               = "user_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.user_lambda_assume_role.json
+}
+
+resource "aws_iam_role" "user_lambda_invocation_role" {
+  name               = "user_lambda_invocation_role"
+  assume_role_policy = data.aws_iam_policy_document.invocation_assume_role_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "user_lambda_logging_attachment" {
@@ -45,4 +87,9 @@ resource "aws_iam_role_policy_attachment" "user_lambda_logging_attachment" {
 resource "aws_iam_role_policy_attachment" "user_lambda_basic_execution" {
   role       = aws_iam_role.user_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "user_lambda_invocation_policy_attachment" {
+  role       = aws_iam_role.user_lambda_invocation_role.name
+  policy_arn = aws_iam_policy.user_lambda_invocation.arn
 }
