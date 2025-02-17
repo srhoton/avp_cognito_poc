595c46c67e211edc6edb67fa690d1e4635c9d18a
Steve Rhoton <steve.rhoton@fullbay.com>
2025-02-16 19:13:27 -0500

    Branch logic stubbed in

--- user-authorizer/src/main/java/com/fullbay/ath/UserAuthorizer.java
+++ user-authorizer/src/main/java/com/fullbay/ath/UserAuthorizer.java
@@ -8,7 +8,6 @@ import com.amazonaws.services.lambda.runtime.events.IamPolicyResponseV1;
 import com.amazonaws.services.lambda.runtime.events.IamPolicyResponseV1.PolicyDocument;
 //import com.amazonaws.services.lambda.runtime.events.IamPolicyResponseV1.Statement;
 import com.nimbusds.jose.*;
-import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.source.*;
 import com.nimbusds.jose.proc.BadJOSEException;
 import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
@@ -16,22 +15,15 @@ import com.nimbusds.jose.proc.JWSKeySelector;
 import com.nimbusds.jose.proc.JWSVerificationKeySelector;
 import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.jwt.*;
-import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.proc.*;
 import jakarta.inject.Named;
-import java.io.*;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
-import java.security.*;
-import java.security.Policy;
-import java.security.interfaces.RSAPublicKey;
 import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
@@ -46,36 +38,80 @@ public class UserAuthorizer
         Context context
     ) {
         LambdaLogger logger = context.getLogger();
-        logger.log("Received event: " + event);
-        logger.log("Received context: " + context);
+        logger.log("Received event: " + event.toString());
+        logger.log("Received context: " + context.toString());
 
         Map<String, String> headers = event.getHeaders();
+        APIGatewayProxyRequestEvent.ProxyRequestContext requestContext =
+            event.getRequestContext();
+        logger.log("Received request context: " + requestContext.toString());
         String token = headers.get("Authorization");
         logger.log("Received token: " + token);
-        logger.log("Token is valid: " + isTokenValid(token, logger));
-        IamPolicyResponseV1.Statement allowStatement =
-            IamPolicyResponseV1.Statement.builder()
-                .withEffect(IamPolicyResponseV1.ALLOW)
-                .withResource(Collections.singletonList("your-resource"))
-                .withAction(IamPolicyResponseV1.EXECUTE_API_INVOKE)
-                .withCondition(
-                    Collections.singletonMap(
-                        "StringEquals",
-                        Collections.singletonMap("aws:username", "exampleUser")
+        Boolean tokenValid = isTokenValid(token, logger);
+        logger.log("Token is valid: " + tokenValid);
+        if (tokenValid) {
+            IamPolicyResponseV1.Statement allowStatement =
+                IamPolicyResponseV1.Statement.builder()
+                    .withEffect(IamPolicyResponseV1.ALLOW)
+                    .withResource(
+                        Collections.singletonList(
+                            context.getInvokedFunctionArn()
+                        )
                     )
-                )
+                    .withAction(IamPolicyResponseV1.EXECUTE_API_INVOKE)
+                    .withCondition(
+                        Collections.singletonMap(
+                            "StringEquals",
+                            Collections.singletonMap(
+                                "aws:username",
+                                requestContext.getIdentity().getCaller()
+                            )
+                        )
+                    )
+                    .build();
+            PolicyDocument policyDocument = PolicyDocument.builder()
+                .withStatement(Collections.singletonList(allowStatement))
+                .withVersion("2012-10-17")
                 .build();
-        PolicyDocument policyDocument = PolicyDocument.builder()
-            .withStatement(Collections.singletonList(allowStatement))
-            .withVersion("2012-10-17")
-            .build();
 
-        IamPolicyResponseV1 response = IamPolicyResponseV1.builder()
-            .withPrincipalId("example-user")
-            .withPolicyDocument(policyDocument)
-            .build();
+            IamPolicyResponseV1 response = IamPolicyResponseV1.builder()
+                .withPrincipalId("example-user")
+                .withPolicyDocument(policyDocument)
+                .build();
 
-        return response;
+            return response;
+        } else {
+            IamPolicyResponseV1.Statement denyStatement =
+                IamPolicyResponseV1.Statement.builder()
+                    .withEffect(IamPolicyResponseV1.DENY)
+                    .withResource(
+                        Collections.singletonList(
+                            context.getInvokedFunctionArn()
+                        )
+                    )
+                    .withAction(IamPolicyResponseV1.EXECUTE_API_INVOKE)
+                    .withCondition(
+                        Collections.singletonMap(
+                            "StringEquals",
+                            Collections.singletonMap(
+                                "aws:username",
+                                requestContext.getIdentity().getCaller()
+                            )
+                        )
+                    )
+                    .build();
+            PolicyDocument policyDocument = PolicyDocument.builder()
+                .withStatement(Collections.singletonList(denyStatement))
+                .withVersion("2012-10-17")
+                .build();
+
+            IamPolicyResponseV1 response = IamPolicyResponseV1.builder()
+                .withPrincipalId("example-user")
+                .withPolicyDocument(policyDocument)
+                .build();
+
+            return response;
+        }
     }
 
     private Boolean isTokenValid(String accessToken, LambdaLogger logger) {
